2cb76e85d93f3b48858cc714c2c8d881e4919212
EliteGams <20193531+EliteGams@users.noreply.github.com>
2020-08-22 00:07:27 -0500

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -9,20 +9,20 @@ router.get('/', (req, res) => {
     author: 'Chris Michael',
     entries: [
       {
-        'LatestAnimeAdded': '/api/v1/LatestAnimeAdded',
-        'LatestEpisodesAdded': '/api/v1/LatestEpisodesAdded',
-        'GetAnimeServers': '/api/v1/GetAnimeServers/:id',
-        'Genres': '/api/v1/Genres/:genre/:sortBy/:page',
-        'Movies': '/api/v1/Movies/:sortBy/:page',
-        'Special': '/api/v1/Special/:sortBy/:page',
-        'Ova': '/api/v1/Ova/:sortBy/:page',
-        'TV': '/api/v1/TV/:sortBy/:page',
-        'AnimeByState': '/api/v1/AnimeByState/:state/:order/:page',
-        'Search': '/api/v1/Search/:query',
-        'AnimeCharacters': '/api/v1/AnimeCharacters/:title',
-        'AnimeTrailers': '/api/v1/AnimeTrailers/:title',
-        'GetAnimeInfo': '/api/v1/GetAnimeInfo/:id/:title',
-        'DownloadLinksByEpsId': '/api/v1/DownloadLinksByEpsId/:epsId'
+        'LatestAnimeAdded': 'https://animeflv-zeta.vercel.app/api/v1/LatestAnimeAdded',
+        'LatestEpisodesAdded': 'https://animeflv-zeta.vercel.app/api/v1/LatestEpisodesAdded',
+        'GetAnimeServers': 'https://animeflv-zeta.vercel.app/api/v1/GetAnimeServers/:id',
+        'Genres': 'https://animeflv-zeta.vercel.app/api/v1/Genres/:genre/:sortBy/:page',
+        'Movies': 'https://animeflv-zeta.vercel.app/api/v1/Movies/:sortBy/:page',
+        'Special': 'https://animeflv-zeta.vercel.app/api/v1/Special/:sortBy/:page',
+        'Ova': 'https://animeflv-zeta.vercel.app/api/v1/Ova/:sortBy/:page',
+        'TV': 'https://animeflv-zeta.vercel.app/api/v1/TV/:sortBy/:page',
+        'AnimeByState': 'https://animeflv-zeta.vercel.app/api/v1/AnimeByState/:state/:order/:page',
+        'Search': 'https://animeflv-zeta.vercel.app/api/v1/Search/:query',
+        'AnimeCharacters': 'https://animeflv-zeta.vercel.app/api/v1/AnimeCharacters/:title',
+        'AnimeTrailers': 'https://animeflv-zeta.vercel.app/api/v1/AnimeTrailers/:title',
+        'GetAnimeInfo': 'https://animeflv-zeta.vercel.app/api/v1/GetAnimeInfo/:id/:title',
+        'DownloadLinksByEpsId': 'https://animeflv-zeta.vercel.app/api/v1/DownloadLinksByEpsId/:epsId'
       }
     ]
   });
